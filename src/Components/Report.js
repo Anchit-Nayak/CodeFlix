b939bb5f30e4b806a1b8bf86d6cd75b228a5cd5e
Anchit <anchitnayak00@gmail.com>
2023-01-29 02:40:04 +0530

    report page added

---
+++ src/Components/Report.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Report =() =>{
+    return(
+        <React.Fragment>
+            <h1>Report</h1>
+        </React.Fragment>
+    )
+
+}
+
+export default Report;
