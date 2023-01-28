847adedb0f1aaedf6b4b6e51c73651a2cff14a55
Anchit <anchitnayak00@gmail.com>
2023-01-28 17:22:35 +0530

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { Container } from '@mui/system';
 import './App.css';
 //Components:
 import NavBar from './Components/Navbar';
 
 function App() {
   return (
+    <Container>
     <div className="App">
       <NavBar/>
     </div>
+    </Container>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,14 +4,13 @@ import { AppBar, Button, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 const NavBar = () => {
     return (
         <React.Fragment>
-        <AppBar>
+        <AppBar sx={{backgroundColor:"#bd0808"}}>
         <Toolbar>
-            <Typography>Inventory</Typography>
+            <Typography sx={{fontFamily:'VT323, monospace', fontSize:"50px",marginLeft:"60px"}}>Inventory</Typography>
             <Tabs sx={{marginLeft:'auto'}}>
-             <Tab label="Report" sx={{color:"inherit"}}/>
-             <Button href="#text-buttons">Link</Button>
+             <Tab label="Report" sx={{fontFamily:'VT323, monospace', fontSize:"30px",color:"inherit"}}/>
             </Tabs>
-            
+            <Button variant="text" sx={{fontFamily:'VT323, monospace', fontSize:"30px",marginLeft:"20px", marginRight:"40px",color:"inherit"}}>SignOut</Button>
         </Toolbar>
         </AppBar>      
         </React.Fragment>
